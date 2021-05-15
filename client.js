12c0e7bcc687ffc6899e0c7577ef0af8f0fc2493
Owen Carruth <ocarr199@gmail.com>
2021-05-15 17:55:43 -0500

    Finished Functional Flex Goals, still need to style

--- client.js
+++ client.js
@@ -1,43 +1,52 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-
+// adding Objects to the table event listener
     $('#submitButton').on("click", submitClicked)
-    $('#submitButton').on("click", submitClicked)
+// delete employee on click of delete button
+    $('#theTable').on('click', '#deleteButton', deleter)
 });
 
-let employees = []
-
+// function that will run when submit button is clicked
 function submitClicked() {
-    let employeeObj = {
-        firstName: $('#firstNameInp').val(),
-        lastName:$('#lastNameInp').val(),
-        ID:$('#idInp').val(),
-        title:$('#titleInp').val(),
-        Annual:$('#annualInp').val()
-    }
-   
-    employees.push(employeeObj);
 
+// appending inputs to the table
 $('#theTable').append(`
-<tr>
-<td>${employeeObj.firstName}</td>
-<td>${employeeObj.firstName}</td>
-<td>${employeeObj.firstName}</td>
-<td>${employeeObj.firstName}</td>
-<td>${employeeObj.firstName}</td>
-<td><button id="deleteButton">Delete</button></td>
+<tr class="tableRows">
+<td> ${$('#firstNameInp').val()}</td>
+<td>${$('#lastNameInp').val()}</td>
+<td>${$('#idInp').val()}</td>
+<td>${$('#titleInp').val()}</td>
+<td class="yearlySal">${$('#annualInp').val()}</td>
+<td><button id="deleteButton" class="btn btn-danger">Delete</button></td>
 </tr>
 `)   
+// Calculate total monthly after person is added
+totalMonthly();
+// Clear inputs after submit button is clicked
+$('.inpBox').val('')
+}
+// function to calculate total monthly costs
 function totalMonthly(){
+    // declaring totalMonthly variable
     let totalMonthly = 0;
-   for(let i = 0; i < employees.length; i++){
-       totalMonthly = (employees[i].Annual * employees.length)/ 12
+// get number of each salary
+$('.yearlySal').each(function(){
+    totalMonthly += (parseFloat($(this).text()/12))
+})
+
+// adding monthly costs to the DOM
+$('#totalMonthly').text(`Total Monthly: $${totalMonthly}`)
+// adding redba
+   if(totalMonthly > 20000){
+       $('#totalMonthly').addClass('warning');
    }
-   console.log(totalMonthly)
 }
-
+function deleter(){
+    $(this).closest('tr').remove()
+    // recalculate total price with person removed
+    totalMonthly();
+    if(totalMonthly < 20000){
+        $('#totalMonthly').removeClass('warning');
+    }
 }
-
-
-
 
