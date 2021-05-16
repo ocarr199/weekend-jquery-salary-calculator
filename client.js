9201c20f40dcae6fe38b7510bcde19b5dc6494f9
Owen Carruth <ocarr199@gmail.com>
2021-05-15 22:50:12 -0500

    Added bootsrap styling, alerts

--- client.js
+++ client.js
@@ -8,7 +8,19 @@ $( document ).ready(function() {
 
 // function that will run when submit button is clicked
 function submitClicked() {
-
+if(isNaN($('#annualInp').val())){
+//     <div class="alert alert-danger d-flex align-items-center" role="alert">
+//   <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+//   <div>
+//     An example danger alert with an icon
+//   </div>
+// </div>
+$('.submitDiv').append(`
+<div class="alert alert-warning " role="alert">
+  <strong>Oh No!</strong> Annual salary can only contain numbers.
+</div>
+`)
+}else{
 // appending inputs to the table
 $('#theTable').append(`
 <tr class="tableRows">
@@ -17,7 +29,7 @@ $('#theTable').append(`
 <td>${$('#idInp').val()}</td>
 <td>${$('#titleInp').val()}</td>
 <td class="yearlySal">${$('#annualInp').val()}</td>
-<td><button id="deleteButton" class="btn btn-danger">Delete</button></td>
+<td><button id="deleteButton" class="btn btn-danger"content>Delete</button></td>
 </tr>
 `)   
 // Calculate total monthly after person is added
@@ -25,28 +37,32 @@ totalMonthly();
 // Clear inputs after submit button is clicked
 $('.inpBox').val('')
 }
+}
 // function to calculate total monthly costs
 function totalMonthly(){
     // declaring totalMonthly variable
     let totalMonthly = 0;
 // get number of each salary
 $('.yearlySal').each(function(){
-    totalMonthly += (parseFloat($(this).text()/12))
+    totalMonthly += Number((parseFloat($(this).text()/12)).toFixed(2))
 })
 
 // adding monthly costs to the DOM
 $('#totalMonthly').text(`Total Monthly: $${totalMonthly}`)
 // adding redba
    if(totalMonthly > 20000){
-       $('#totalMonthly').addClass('warning');
-   }
+       $('#totalMonthly').addClass('alert alert-danger');
+       $('#totalMonthly').append('<p> Monthly Spending too high!')
+   }else {
+    $('#totalMonthly').removeClass('alert alert-danger');
 }
+}
+
+// delete table rows function
 function deleter(){
     $(this).closest('tr').remove()
     // recalculate total price with person removed
     totalMonthly();
-    if(totalMonthly < 20000){
-        $('#totalMonthly').removeClass('warning');
-    }
+    
 }
 
