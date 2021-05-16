477730da8db6897e41b9a3220c91f17bd3a624dd
Owen Carruth <ocarr199@gmail.com>
2021-05-16 14:46:10 -0500

    Project Completd

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Project Name
 
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+Monthly Spending Calculator
 
 ## Description
+This is a project to help a company budget their employees salaries. There are inputs for First Name,
+Last Name, Employee ID, Job Title and Annual Salary. These are all added to a table and the annual salary for each employee is converted into their monthly salary and added to the total monthly expense below the table.
+If not all fields are completed, or the annual salary is not a number the user gets a dismissable alert making them aware of their specific mistake. If the total monthly expense goes above 20 thousand the 
+total monthly expense recieves a red background and a warning that they are spending too much. 
+Each employee in the list has a delete button and if someone is deleted then the monthly expense is adjusted and if this results in monthly expnense returning below 20 thousand then the red background and warning will
+be removed.
+This project uses HTML, CSS, Javascript, jQuery, Bootstrap.css and Bootstrap.js
 
-Your project description goes here. What problem did you solve? How did you solve it?
+Thanks for looking at my project :)
+
+
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- client.js
+++ client.js
@@ -1,68 +1,72 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-// adding Objects to the table event listener
+$(document).ready(function () {
+    console.log("ready!");
+    // adding Objects to the table event listener
     $('#submitButton').on("click", submitClicked)
-// delete employee on click of delete button
+    // delete employee on click of delete button
     $('#theTable').on('click', '#deleteButton', deleter)
+
+
 });
 
 // function that will run when submit button is clicked
 function submitClicked() {
-if(isNaN($('#annualInp').val())){
-//     <div class="alert alert-danger d-flex align-items-center" role="alert">
-//   <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-//   <div>
-//     An example danger alert with an icon
-//   </div>
-// </div>
-$('.submitDiv').append(`
-<div class="alert alert-warning " role="alert">
-  <strong>Oh No!</strong> Annual salary can only contain numbers.
+    if (!$('.inpBox').val()) {
+        // Alert for if they didn't complete all inputs
+        $('.submitDiv').append(`
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Whoops!</strong> Make sure to complete all inputs.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
+ `)
+    } else if (isNaN($('#annualInp').val()) || (!$('#annualInp').val())) {
+        $('.submitDiv').append(`
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+   <strong>OOPS!</strong> Annual Salary can only contain numbers.
+   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+ </div>
 `)
-}else{
-// appending inputs to the table
-$('#theTable').append(`
+    } else {
+        // appending inputs to the table
+        $('#theTable').append(`
 <tr class="tableRows">
 <td> ${$('#firstNameInp').val()}</td>
 <td>${$('#lastNameInp').val()}</td>
 <td>${$('#idInp').val()}</td>
 <td>${$('#titleInp').val()}</td>
-<td class="yearlySal">${$('#annualInp').val()}</td>
+<td class="yearlySal"> ${$('#annualInp').val()}</td>
 <td><button id="deleteButton" class="btn btn-danger"content>Delete</button></td>
 </tr>
-`)   
-// Calculate total monthly after person is added
-totalMonthly();
-// Clear inputs after submit button is clicked
-$('.inpBox').val('')
-}
+`)
+        // Calculate total monthly after person is added
+        totalMonthly();
+        // Clear inputs after submit button is clicked
+        $('.inpBox').val('')
+    }
 }
 // function to calculate total monthly costs
-function totalMonthly(){
+function totalMonthly() {
     // declaring totalMonthly variable
     let totalMonthly = 0;
-// get number of each salary
-$('.yearlySal').each(function(){
-    totalMonthly += Number((parseFloat($(this).text()/12)).toFixed(2))
-})
+    // get number of each salary
+    $('.yearlySal').each(function () {
+        totalMonthly += Number((parseFloat($(this).text() / 12)).toFixed(2))
+    })
 
-// adding monthly costs to the DOM
-$('#totalMonthly').text(`Total Monthly: $${totalMonthly}`)
-// adding redba
-   if(totalMonthly > 20000){
-       $('#totalMonthly').addClass('alert alert-danger');
-       $('#totalMonthly').append('<p> Monthly Spending too high!')
-   }else {
-    $('#totalMonthly').removeClass('alert alert-danger');
-}
+    // adding monthly costs to the DOM
+    $('#totalMonthly').text(`Total Monthly Expense: $${totalMonthly}`)
+    // adding redba
+    if (totalMonthly > 20000) {
+        $('#totalMonthly').addClass('alert alert-danger');
+        $('#totalMonthly').text(`Total Monthly Expense: $${totalMonthly}   Monthly Spending Too High!`)
+    } else {
+        $('#totalMonthly').removeClass('alert alert-danger');
+    }
 }
 
 // delete table rows function
-function deleter(){
+function deleter() {
     $(this).closest('tr').remove()
     // recalculate total price with person removed
     totalMonthly();
-    
-}
 
+}
